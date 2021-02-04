4fc51f34dc16d457e72333efedda756786d58d8f
disrupted <hi@salomonpopp.me>
2021-02-04 13:50:28 +0100

    Remove abstractmethod where not permitted

--- backend/streams_explorer/core/extractor/extractor.py
+++ backend/streams_explorer/core/extractor/extractor.py
@@ -1,5 +1,3 @@
-from abc import abstractmethod
-
 from kubernetes.client import V1beta1CronJob
 
 
@@ -7,14 +5,11 @@ class Extractor:
     sources = None
     sinks = None
 
-    @abstractmethod
     def on_streaming_app_env_parsing(self, env, streaming_app_name: str):
         pass
 
-    @abstractmethod
     def on_connector_config_parsing(self, config, connector_name: str):
         pass
 
-    @abstractmethod
     def on_cron_job_parsing(self, cron_job: V1beta1CronJob):
         pass
