a10d8158e7216e8b61eda9cf6be26003e72c9b9e
disrupted <hi@salomonpopp.me>
2021-02-04 14:34:30 +0100

    Add ES sink only for ElasticsearchSinkConnector

--- backend/streams_explorer/core/extractor/default/elasticsearch_sink.py
+++ backend/streams_explorer/core/extractor/default/elasticsearch_sink.py
@@ -9,12 +9,13 @@ class ElasticsearchSink(Extractor):
         self.sinks: List[Sink] = []
 
     def on_connector_config_parsing(self, config, connector_name):
-        index = config.get("transforms.changeTopic.replacement")
-        if index:
-            self.sinks.append(
-                Sink(
-                    name=index,
-                    node_type="elasticsearch-index",
-                    source=connector_name,
+        if "ElasticsearchSinkConnector" in config.get("connector.class"):
+            index = config.get("transforms.changeTopic.replacement")
+            if index:
+                self.sinks.append(
+                    Sink(
+                        name=index,
+                        node_type="elasticsearch-index",
+                        source=connector_name,
+                    )
                 )
-            )
