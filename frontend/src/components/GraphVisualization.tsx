b2f82df7fe3b92b2be859bad618032b2007b8f51
Victor Kuenstler <victor.kuenstler@bakdata.com>
2021-02-05 14:58:45 +0100

    Add edge animation

--- frontend/src/components/GraphVisualization.tsx
+++ frontend/src/components/GraphVisualization.tsx
@@ -8,9 +8,10 @@ import {
   NodeConfig,
   IG6GraphEvent,
 } from "@antv/g6/lib/types";
-import INode from "@antv/g6/lib/interface/item";
+import { IEdge, INode } from "@antv/g6/lib/interface/item";
 import Graph from "@antv/g6/lib/graph/graph";
 import "./MetricCustomNode";
+import "./DashedEdge";
 import { millify } from "millify";
 
 interface GraphVisualizationProps {
@@ -73,11 +74,27 @@ function updateNodeMetrics(graph: Graph, metrics: Metric[]) {
     ]
       .filter(Boolean)
       .join("  ");
-    let node = graph.findById(metric.node_id);
+
+    let node = graph.findById(metric.node_id) as INode;
     if (node) {
       graph.updateItem(node, {
         metric: metricsString,
       });
+
+      // update edge animation
+      const nodeType = node.getModel().node_type;
+      if (nodeType === "topic" || nodeType === "error-topic") {
+        node.getInEdges().forEach((edge: IEdge, index: number) => {
+          graph.updateItem(edge, {
+            type: metric.messages_in ? "line-dash" : "cubic-horizontal",
+          });
+        });
+        node.getOutEdges().forEach((edge: IEdge, index: number) => {
+          graph.updateItem(edge, {
+            type: metric.messages_out ? "line-dash" : "cubic-horizontal",
+          });
+        });
+      }
     }
   });
 }
@@ -100,7 +117,7 @@ const GraphVisualization = ({
 
   const mouseEnterCallback = useCallback(
     (e: IG6GraphEvent) => {
-      const node = e.item as INode.INode;
+      const node = e.item as INode;
       graph?.setItemState(node, "hover", true);
     },
     [graph]
@@ -108,7 +125,7 @@ const GraphVisualization = ({
 
   const mouseLeaveCallback = useCallback(
     (e: IG6GraphEvent) => {
-      const node = e.item as INode.INode;
+      const node = e.item as INode;
       graph?.setItemState(node, "hover", false);
     },
     [graph]
@@ -120,7 +137,7 @@ const GraphVisualization = ({
       clickNodes?.forEach((cn) => {
         graph?.setItemState(cn, "click", false);
       });
-      const node = e.item as INode.INode;
+      const node = e.item as INode;
       graph?.setItemState(node, "click", true);
     },
     [graph]
@@ -155,6 +172,7 @@ const GraphVisualization = ({
         node.icon = icon;
       }
     });
+
     currentGraph?.data(data as GraphData);
     currentGraph?.render();
 
