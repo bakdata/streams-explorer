f04f394fcf4bc741c9f8c370c5160b50a3ef12b1
Salomon Popp <mail@salomonpopp.me>
2021-02-16 15:09:40 +0100

    Sanitize Kafka Connect config secrets (#60)

--- backend/streams_explorer/core/services/kafkaconnect.py
+++ backend/streams_explorer/core/services/kafkaconnect.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Dict, List, Set
 
 import requests
 from loguru import logger
@@ -8,6 +8,7 @@ from streams_explorer.extractors import extractor_container
 from streams_explorer.models.kafka_connector import KafkaConnector
 
 url = settings.kafkaconnect.url
+protected_keys: Dict[str, Set[str]] = {}
 
 
 class KafkaConnect:
@@ -18,13 +19,61 @@ class KafkaConnect:
 
     @staticmethod
     def get_connector_info(connector_name: str) -> dict:
-        logger.info(f"Get connector information for {connector_name}")
+        logger.info("Get connector information for {}", connector_name)
         response = requests.get(f"{url}/connectors/{connector_name}")
         info: dict = response.json()
         return info
 
     @staticmethod
+    def get_connector_config(connector_name: str) -> dict:
+        info = KafkaConnect.get_connector_info(connector_name)
+        return KafkaConnect.sanitize_connector_config(info["config"])
+
+    @staticmethod
+    def extract_connector_class_basename(connector_class: str) -> str:
+        if "." in connector_class:
+            return connector_class.rsplit(".", 1)[-1]
+        return connector_class
+
+    @staticmethod
+    def retrieve_connector_class_protected_keys(
+        connector_class: str, config: dict
+    ) -> None:
+        response = requests.put(
+            f"{url}/connector-plugins/{connector_class}/config/validate",
+            json=config,
+        )
+        if not response.ok:
+            logger.warning(
+                'Couldn\'t retrieve connector class validation for "{}": {}',
+                connector_class,
+                response,
+            )
+            return
+        data = response.json()
+        protected_keys[connector_class] = {
+            config["value"]["name"]
+            for config in data["configs"]
+            if config["definition"]["type"] == "PASSWORD"
+        }
+
+    @staticmethod
+    def sanitize_connector_config(config: dict) -> dict:
+        connector_class = KafkaConnect.extract_connector_class_basename(
+            config["connector.class"]
+        )
+        if connector_class not in protected_keys:
+            KafkaConnect.retrieve_connector_class_protected_keys(
+                connector_class, config
+            )
+        for key in protected_keys[connector_class].intersection(config):
+            config[key] = "[hidden]"
+            logger.debug('Sanitized connector config "{}"', key)
+        return config
+
+    @staticmethod
     def connectors() -> List[KafkaConnector]:
+        protected_keys.clear()
         connectors = KafkaConnect.get_connectors()
         out = []
         for name in connectors:
