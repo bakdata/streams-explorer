2cff7be34cc706775151ee81cf7cf6b2a99c2e3f
disrupted <hi@salomonpopp.me>
2021-02-18 17:11:15 +0100

    Remove bool from SE

--- backend/streams_explorer/api/routes/kubernetes_health.py
+++ backend/streams_explorer/api/routes/kubernetes_health.py
@@ -1,15 +1,8 @@
-from fastapi import APIRouter, Depends
-
-from streams_explorer.api.dependencies.streams_explorer import (
-    get_streams_explorer_from_request,
-)
-from streams_explorer.streams_explorer import StreamsExplorer
+from fastapi import APIRouter
 
 router = APIRouter()
 
 
 @router.get("", status_code=200)
-async def ready(
-    streams_explorer: StreamsExplorer = Depends(get_streams_explorer_from_request),
-):
-    return {"ready": streams_explorer.ready}
+async def ready():
+    return {"ready": True}
