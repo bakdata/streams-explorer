f27029047d4c1b75c625ee1dfaa02a2f729d1e70
disrupted <hi@salomonpopp.me>
2021-02-22 14:21:31 +0100

    Fix missing href warning

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -141,7 +141,7 @@ const App: React.FC = () => {
               <Menu.Item style={{ float: "right" }}>
                 Metrics refresh:&nbsp;
                 <Dropdown overlay={menuRefresh}>
-                  <a>
+                  <a href={"/#"}>
                     {refreshIntervals[refreshInterval]} <DownOutlined />
                   </a>
                 </Dropdown>
