69027f5900441f4cab7891f7d454cd28e29cfc9e
Salomon Popp <salomon.popp@bakdata.com>
2021-05-05 13:40:05 +0200

    Use state for input value

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -32,6 +32,7 @@ const App: React.FC = () => {
   const ALL_PIPELINES = "all pipelines";
   const [currentPipeline, setCurrentPipeline] = useState(ALL_PIPELINES);
   const [detailNode, setDetailNode] = useState<string | null>(null);
+  const [inputValue, setInputValue] = useState<string | undefined>(undefined);
   const [focusedNode, setFocusedNode] = useState<string | null>(null);
   const [searchWidth, setSearchWidth] = useState<number>(300);
   const history = useHistory();
@@ -107,6 +108,7 @@ const App: React.FC = () => {
     }
     const focusNode = params.get("focus-node");
     if (focusNode) {
+      setInputValue(focusNode);
       setFocusedNode(focusNode);
       setDetailNode(focusNode);
     }
@@ -179,7 +181,11 @@ const App: React.FC = () => {
                       .toUpperCase()
                       .indexOf(inputValue.toUpperCase()) !== -1
                   }
-                  value={focusedNode ? focusedNode : undefined}
+                  value={inputValue}
+                  onChange={(value: string) => {
+                    console.log(value);
+                    setInputValue(value);
+                  }}
                   onSelect={(nodeId: string) => {
                     setFocusedNode(nodeId);
                     setDetailNode(nodeId);
