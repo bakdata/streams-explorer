dbc77d69b592af8b0917837a3b41b9d62064b3eb
Salomon Popp <salomon.popp@bakdata.com>
2021-05-19 17:32:01 +0200

    Test updating focus-node

--- frontend/src/UrlParams.test.tsx
+++ frontend/src/UrlParams.test.tsx
@@ -192,6 +192,7 @@ describe("handles url parameters", () => {
     ).toBeInTheDocument();
     expect(nockPipelineGraph.isDone()).toBeTruthy(); // specific graph endpoint was called
     expect(nockGraph.isDone()).toBeFalsy();
+    expect(nockNode.isDone()).toBeFalsy();
 
     const nodeSelect = getByTestId("node-select");
     const input = within(nodeSelect).getByRole("combobox") as HTMLInputElement;
@@ -218,19 +219,18 @@ describe("handles url parameters", () => {
     await waitForElement(() => getByTestId("graph"));
     expect(asFragment()).toMatchSnapshot();
 
-    await wait(() => {
+    wait(() => {
       const currentPipeline = getByTestId("pipeline-current");
       expect(
         within(currentPipeline).getByText("all pipelines")
       ).toBeInTheDocument();
-      // expect(nockPipelineGraph.isDone()).toBeTruthy(); // specific graph endpoint was called
-      // expect(nockGraph.isDone()).toBeFalsy();
 
       const nodeSelect = getByTestId("node-select");
       const input = within(nodeSelect).getByRole(
         "combobox"
       ) as HTMLInputElement;
       expect(input).toHaveValue("test-app");
+      expect(nockNode.isDone()).toBeTruthy();
     });
   });
 
@@ -249,7 +249,7 @@ describe("handles url parameters", () => {
 
     await waitForElement(() => getByTestId("graph"));
 
-    await wait(() => {
+    wait(() => {
       // check pipeline set to all
       const currentPipeline = getByTestId("pipeline-current");
       expect(
@@ -278,22 +278,23 @@ describe("handles url parameters", () => {
       </RestfulProvider>
     );
 
-    // const currentPipeline = getByTestId("pipeline-current");
-    // expect(
-    //   within(currentPipeline).getByText("test-pipeline")
-    // ).toBeInTheDocument();
-    // expect(nockPipelineGraph.isDone()).toBeTruthy(); // specific graph endpoint was called
-    // expect(nockGraph.isDone()).toBeFalsy();
-
     await waitForElement(() => getByTestId("graph"));
     const nodeSelect = getByTestId("node-select");
     const input = within(nodeSelect).getByRole("combobox") as HTMLInputElement;
     expect(input).toBeEmpty();
 
+    const nockTopic = nock("http://localhost")
+      .get("/api/node/test-topic")
+      .reply(200, {
+        node_id: "test-topic",
+        node_type: "topic",
+        info: [],
+      });
+    expect(nockTopic.isDone()).toBeFalsy();
+
     // -- set focus-node through UI
-    // expect(nockNode.isDone()).toBeFalsy();
-    await wait(() => {
-      fireEvent.change(input, { target: { value: "test-app" } });
+    fireEvent.change(input, { target: { value: "test-app" } });
+    wait(() => {
       expect(input).toHaveValue("test-app");
       let options = getAllByTestId("node-option");
       expect(options).toHaveLength(1);
@@ -306,9 +307,24 @@ describe("handles url parameters", () => {
       expect(nockNode.isDone()).toBeTruthy();
     });
 
-    // -- set pipeline through UI
+    // -- update focus-node through UI
+    fireEvent.change(input, { target: { value: "test-topic" } });
+    wait(() => {
+      expect(input).toHaveValue("test-topic");
+      let options = getAllByTestId("node-option");
+      expect(options).toHaveLength(2);
+      fireEvent.click(options[0]);
+
+      // -- check focus-node updated, pipeline still present
+      expect(getByTestId("location-search")).toHaveTextContent(
+        "?pipeline=test-pipeline&focus-node=test-topic"
+      );
+      expect(nockTopic.isDone()).toBeTruthy();
+    });
+
+    // -- (re-)set pipeline through UI
     fireEvent.mouseOver(getByTestId("pipeline-current"));
-    await wait(() => {
+    wait(() => {
       expect(getByTestId("pipeline-select")).toBeInTheDocument();
       const pipeline = getByText("all pipelines");
       expect(pipeline).toBeInTheDocument();
