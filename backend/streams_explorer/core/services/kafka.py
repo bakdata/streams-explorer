25de4d335278afeba28d654a48c1cc19161236b2
Salomon Popp <salomon.popp@bakdata.com>
2021-08-30 17:56:19 +0200

    Extract methods

--- backend/streams_explorer/core/services/kafka.py
+++ backend/streams_explorer/core/services/kafka.py
@@ -11,14 +11,23 @@ class Kafka:
         self.__client: AdminClient = AdminClient(settings.kafka.config)
         self.__timeout: int = 1
 
-    def __get_config(self, resource: ConfigResource) -> ValuesView:
+    def __describe_config_values(self, resource: ConfigResource) -> ValuesView:
         configs = self.__client.describe_configs(resources=[resource])
         return configs.values()
 
-    def get_topic_config(self, topic: str) -> dict:
-        topic_config = ConfigResource(ConfigResource.Type.TOPIC, topic)
-
-        for c in concurrent.futures.as_completed(iter(self.__get_config(topic_config))):
+    def __get_resource(self, resource: ConfigResource) -> Dict[str, ConfigEntry]:
+        for c in concurrent.futures.as_completed(
+            iter(self.__describe_config_values(resource))
+        ):
             result: Dict[str, ConfigEntry] = c.result(timeout=self.__timeout)
-            return {config.name: config.value for config in result.values()}
+            return result
         return {}
+
+    @staticmethod
+    def __format_values(values: ValuesView[ConfigEntry]) -> Dict[str, str]:
+        return {config.name: config.value for config in values}
+
+    def get_topic_config(self, topic: str) -> Dict[str, str]:
+        topic_resource = ConfigResource(ConfigResource.Type.TOPIC, topic)
+        result = self.__get_resource(topic_resource)
+        return self.__format_values(result.values())
