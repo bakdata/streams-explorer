d4c984953d549d0de83d9b3f0fcd65eb4f5f0154
Salomon Popp <salomon.popp@bakdata.com>
2021-08-30 17:17:23 +0200

    Create Kafka service

---
+++ backend/streams_explorer/core/services/kafka.py
@@ -0,0 +1,25 @@
+import concurrent.futures
+from typing import Dict, ValuesView
+
+from confluent_kafka.admin import AdminClient, ConfigEntry, ConfigResource
+
+from streams_explorer.core.config import settings
+
+
+class Kafka:
+    def __init__(self):
+        self.__client: AdminClient = AdminClient(
+            {"bootstrap.servers": settings.kafka.url}
+        )
+        self.__timeout: int = 1
+
+    def __get_config(self, resource: ConfigResource) -> ValuesView:
+        configs = self.__client.describe_configs(resources=[resource])
+        return configs.values()
+
+    def get_topic_config(self, topic: str) -> dict:
+        topic_config = ConfigResource(ConfigResource.Type.TOPIC, topic)
+        for c in concurrent.futures.as_completed(iter(self.__get_config(topic_config))):
+            result: Dict[str, ConfigEntry] = c.result(timeout=self.__timeout)
+            return {k: v.value for k, v in result.items()}
+        return {}
