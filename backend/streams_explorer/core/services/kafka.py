d729c3aae7e8f943a7c7596471dfd61c7059acb2
Salomon Popp <salomon.popp@bakdata.com>
2021-09-01 16:01:09 +0200

    Set type hint for dict value

--- backend/streams_explorer/core/services/kafka.py
+++ backend/streams_explorer/core/services/kafka.py
@@ -1,5 +1,5 @@
 import concurrent.futures
-from typing import Dict, List
+from typing import Any, Dict, List
 
 from confluent_kafka.admin import AdminClient, ConfigEntry, ConfigResource
 
@@ -20,13 +20,13 @@ class Kafka:
             return list(configs.values())
         return []
 
-    def get_topic_config(self, topic: str) -> Dict[str, str]:
+    def get_topic_config(self, topic: str) -> Dict[str, Any]:
         topic_resource = ConfigResource(ConfigResource.Type.TOPIC, topic)
         result = self.__get_resource(topic_resource)
         return self.format_values(result)
 
     @staticmethod
-    def format_values(values: List[ConfigEntry]) -> Dict[str, str]:
+    def format_values(values: List[ConfigEntry]) -> Dict[str, Any]:
         return {
             config.name: config.value for config in values if not config.is_sensitive
         }
