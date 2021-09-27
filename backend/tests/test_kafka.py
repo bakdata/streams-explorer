c0125a543e6d6c50ece643333e45b21a03389564
Salomon Popp <mail@salomonpopp.me>
2021-09-27 14:14:49 +0200

    Display Kafka topic partition count (#172)

--- backend/tests/test_kafka.py
+++ backend/tests/test_kafka.py
@@ -1,9 +1,12 @@
-from typing import List
+from dataclasses import dataclass
+from typing import Dict, List, Optional
 
 import pytest
-from confluent_kafka.admin import ConfigEntry, ConfigResource
+from confluent_kafka.admin import ConfigEntry, ConfigResource, PartitionMetadata
 
 from streams_explorer.core.services.kafka import Kafka
+
+test_topic = "test-topic"
 
 
 class TestKafka:
@@ -20,7 +23,7 @@ class TestKafka:
         kafka = Kafka()
 
         def mock_get_resource(resource: ConfigResource, *args) -> List[ConfigEntry]:
-            if resource == ConfigResource(ConfigResource.Type.TOPIC, "test-topic"):
+            if resource == ConfigResource(ConfigResource.Type.TOPIC, test_topic):
                 return [
                     ConfigEntry(name="cleanup.policy", value="delete"),
                     ConfigEntry(name="retention.ms", value="-1"),
@@ -28,11 +31,32 @@ class TestKafka:
             return []
 
         monkeypatch.setattr(kafka, "_Kafka__get_resource", mock_get_resource)
+
+        @dataclass
+        class MockTopicMetadata:
+            topic: str
+            partitions: Dict[int, PartitionMetadata]
+
+        def mock_get_topic(topic: str) -> Optional[MockTopicMetadata]:
+            if topic == test_topic:
+                meta = PartitionMetadata()
+                partitions = {i: meta for i in range(10)}
+                return MockTopicMetadata(test_topic, partitions)
+            return None
+
+        monkeypatch.setattr(kafka, "_Kafka__get_topic", mock_get_topic)
+
         return kafka
 
-    def test_get_topic_config(self, kafka):
-        assert kafka.get_topic_config("test-topic") == {
+    def test_get_topic_config(self, kafka: Kafka):
+        assert kafka.get_topic_config(test_topic) == {
             "cleanup.policy": "delete",
             "retention.ms": "-1",
         }
         assert kafka.get_topic_config("doesnt-exist") == {}
+
+    def test_get_topic_partitions(self, kafka: Kafka):
+        partitions = kafka.get_topic_partitions(test_topic)
+        assert type(partitions) is dict
+        assert len(partitions) == 10
+        assert kafka.get_topic_partitions("doesnt-exist") is None
