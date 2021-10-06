9cc5b4d75dba4479279676de65460f414231bec5
Salomon Popp <mail@salomonpopp.me>
2021-10-06 12:33:23 +0200

    Improve settings validation (#174)

--- backend/streams_explorer/core/config.py
+++ backend/streams_explorer/core/config.py
@@ -1,11 +1,11 @@
-from typing import Dict, List
+from typing import Any, Dict, List
 
 from dynaconf import Dynaconf, Validator
 
 APP_NAME = "Streams Explorer"
 API_PREFIX = "/api"
 
-settings = Dynaconf(
+settings: Any = Dynaconf(
     envvar_prefix="se",
     settings_files=["settings.yaml"],
     load_dotenv=True,
@@ -20,12 +20,15 @@ settings = Dynaconf(
         Validator("k8s.labels", must_exist=True, is_type_of=list),
         Validator("k8s.pipeline.label", must_exist=True, is_type_of=str),
         Validator("k8s.consumer_group_annotation", must_exist=True, is_type_of=str),
-        Validator("kafka.enable", must_exist=True, is_type_of=bool),
-        Validator(
-            "kafka.config",
-            must_exist=True,
-            is_type_of=dict,
-            condition=lambda v: type(v.get("bootstrap.servers")) is str,
+        Validator("kafka.enable", eq=False, default=False)
+        | (
+            Validator("kafka.enable", eq=True)
+            & Validator(
+                "kafka.config",
+                is_type_of=dict,
+                must_exist=True,
+                condition=lambda v: type(v.get("bootstrap.servers")) is str,
+            )
         ),
         Validator("kafka.displayed_information", is_type_of=list, default=[]),
         Validator("node_info.cache_ttl", must_exist=True, is_type_of=int),
@@ -35,11 +38,11 @@ settings = Dynaconf(
         Validator("prometheus.url", must_exist=True, is_type_of=str),
         Validator("grafana.dashboards.topics", is_type_of=str),
         Validator("grafana.dashboards.consumergroups", is_type_of=str),
-        Validator("akhq.enable", is_type_of=bool),
-        Validator("kowl.enable", is_type_of=bool),
+        Validator("akhq.enable", is_type_of=bool, default=False),
+        Validator("kowl.enable", is_type_of=bool, default=False),
         Validator("akhq.enable", eq=False) | Validator("kowl.enable", eq=False),
-        Validator("kibanalogs.enable", is_type_of=bool),
-        Validator("loki.enable", is_type_of=bool),
+        Validator("kibanalogs.enable", is_type_of=bool, default=False),
+        Validator("loki.enable", is_type_of=bool, default=False),
         Validator("kibanalogs.enable", eq=False) | Validator("loki.enable", eq=False),
         Validator("plugins.path", must_exist=True, is_type_of=str),
         Validator("plugins.extractors.default", must_exist=True, is_type_of=bool),
