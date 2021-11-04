b96ab519264567af6bbef27a0ce296830df63003
disrupted <hi@salomonpopp.me>
2021-11-04 20:30:50 +0100

    Init tests

---
+++ __tests__/index.test.tsx
@@ -0,0 +1,80 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { render } from "@testing-library/react";
+import nock from "nock";
+import React from "react";
+import App from "../src/App";
+
+// -- Mock GraphVisualization
+jest.mock("../src/components/GraphVisualization", () => {
+  return function DummyGraphVisualization() {
+    return <div data-testid="graph"></div>;
+  };
+});
+
+function mockBackendGraph(persist?: boolean, pipelineName?: string) {
+  return nock("http://localhost")
+    .persist(persist)
+    .get(`/api/graph${pipelineName ? "?pipeline_name=" + pipelineName : ""}`)
+    .reply(200, {
+      directed: true,
+      multigraph: false,
+      graph: {},
+      nodes: [
+        {
+          id: "test-app",
+          label: "test-app-name",
+          node_type: "streaming-app",
+          icon: null,
+          x: 0,
+          y: 0,
+        },
+        {
+          id: "test-topic",
+          label: "test-topic-name",
+          node_type: "topic",
+          icon: null,
+          x: 10,
+          y: 0,
+        },
+      ],
+      edges: [
+        {
+          source: "test-app",
+          target: "test-topic",
+        },
+      ],
+    });
+}
+
+describe("Streams Explorer", () => {
+  beforeAll(() => {
+    Object.defineProperty(window, "matchMedia", {
+      writable: true,
+      value: jest.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // Deprecated
+        removeListener: jest.fn(), // Deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    });
+
+    // disable resize observer
+    (window as any).ResizeObserver = class MockResizeObserver {
+      observe() {}
+      unobserve() {}
+      disconnect() {}
+    };
+  });
+
+  mockBackendGraph(true);
+  it("renders without crashing", () => {
+    render(<App />);
+  });
+});
