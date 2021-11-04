c2b8d2786d56f26f4ae0878c386844a6b4c927c5
disrupted <hi@salomonpopp.me>
2021-11-04 16:25:59 +0100

    Add G6 component

---
+++ pages/G6component.tsx
@@ -0,0 +1,55 @@
+import G6 from "@antv/g6";
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom";
+
+const G6component = () => {
+  const ref = React.useRef(null);
+  let graph = null;
+
+  useEffect(() => {
+    if (!graph) {
+      let data = {
+        nodes: [
+          {
+            id: "node1",
+            x: 100,
+            y: 100,
+          },
+          {
+            id: "node2",
+            x: 200,
+            y: 100,
+          },
+        ],
+        edges: [
+          {
+            source: "node1",
+            target: "node2",
+          },
+        ],
+      };
+
+      graph = new G6.Graph({
+        container: ReactDOM.findDOMNode(ref.current),
+        width: "100%",
+        height: "100%",
+        renderer: "svg",
+        modes: {
+          default: ["zoom-canvas", "drag-node", "drag-canvas"],
+        },
+        minZoom: 0.5,
+        maxZoom: 3,
+      });
+      graph.data(data);
+      graph.render();
+    }
+  }, []);
+  return (
+    <div
+      ref={ref}
+      style={{ width: 1000, height: 1000, border: "1px solid #ccc" }}
+    ></div>
+  );
+};
+
+export default G6component;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/global.css'
-import { AppProps } from 'next/app'
+import "../styles/global.css";
+import { AppProps } from "next/app";
+import dynamic from "next/dynamic";
+
+const G6component = dynamic(() => import("../pages/G6component"), {
+  ssr: false,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // return <Component {...pageProps} />
+  return (
+    <div>
+      <G6component {...pageProps} />
+    </div>
+  );
 }
