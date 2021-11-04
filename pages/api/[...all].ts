c120ab7e58e4a20db9b94bfab1434a427fda41dc
disrupted <hi@salomonpopp.me>
2021-11-04 18:42:56 +0100

    Use native middleware instead of next-http-proxy-middleware

--- pages/api/[...all].ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import httpProxyMiddleware from "next-http-proxy-middleware";
-
-export default (req: NextApiRequest, res: NextApiResponse) =>
-  httpProxyMiddleware(req, res, {
-    target: "http://localhost:8000",
-  });
