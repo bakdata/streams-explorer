47ec984613b28a9edacff28837c7b284309e4542
disrupted <hi@salomonpopp.me>
2021-11-04 17:18:57 +0100

    Proxy api requests to backend

---
+++ pages/api/[...all].ts
@@ -0,0 +1,7 @@
+import { NextApiResponse, NextApiRequest } from "next";
+import httpProxyMiddleware from "next-http-proxy-middleware";
+
+export default (req: NextApiRequest, res: NextApiResponse) =>
+  httpProxyMiddleware(req, res, {
+    target: "http://localhost:8000",
+  });
