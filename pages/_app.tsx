358ec936f949dacd8fbdf270430965a44b7193ca
disrupted <hi@salomonpopp.me>
2021-11-04 16:34:28 +0100

    Remove example data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import { AppProps } from "next/app";
-import "../styles/global.css";
-import G6component from "../components/Graph";
+import Graph from "../components/Graph";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <G6component {...pageProps} />;
+  return <Graph {...pageProps} />;
 }
