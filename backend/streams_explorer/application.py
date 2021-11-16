2472396348f67a2996a72d3ee510d709408e5c8d
Salomon Popp <salomon.popp@bakdata.com>
2021-11-16 19:35:12 +0100

    Had a better idea

--- backend/streams_explorer/application.py
+++ backend/streams_explorer/application.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from starlette.responses import FileResponse
 from starlette.staticfiles import StaticFiles
 
 from streams_explorer.api.routes import api
@@ -8,17 +7,8 @@ from streams_explorer.core.config import API_PREFIX, APP_NAME
 
 def get_application() -> FastAPI:
     app = FastAPI(title=APP_NAME)
-    app.mount(
-        "/_next/static", StaticFiles(directory="_next/static", html=True), name="static"
-    )
-
-    @app.get("/", include_in_schema=False)
-    async def index():
-        return FileResponse("index.html")
-
-    @app.get("/{path}", include_in_schema=False)
-    async def public(path: str):
-        return FileResponse(path)
-
     app.include_router(api.router, prefix=API_PREFIX)
+
+    app.mount("/", StaticFiles(directory="static", html=True), name="static")
+
     return app
