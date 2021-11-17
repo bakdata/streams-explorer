6289294d4173b2ca4a43f3dfc72abda512c4e3b9
Salomon Popp <salomon.popp@bakdata.com>
2021-11-17 13:45:28 +0100

    Load detail styles from CSS module

--- frontend/components/Schema.tsx
+++ frontend/components/Schema.tsx
@@ -6,6 +6,7 @@ import {
   useGetNodeSchemaApiNodeNodeIdSchemaVersionGet,
   useGetNodeSchemaVersionsApiNodeNodeIdSchemaGet,
 } from "./api/fetchers";
+import style from "./Details.module.css";
 
 interface SchemaProps {
   nodeId: string;
@@ -59,7 +60,7 @@ const Schema = ({ nodeId }: SchemaProps) => {
 
   if (schemaLoading || versionsLoading) {
     return (
-      <div className="loadingSpinnerContainer">
+      <div className={style.loadingSpinnerContainer}>
         <Spin tip="Loading schema..." />
       </div>
     );
@@ -92,7 +93,7 @@ const Schema = ({ nodeId }: SchemaProps) => {
             v{schemaVersion} <DownOutlined />
           </Button>
         </Dropdown>
-        <div className="jsonDetail" data-testid="schema">
+        <div className={style.jsonDetail} data-testid="schema">
           <ReactJson
             name={false}
             src={schema}
