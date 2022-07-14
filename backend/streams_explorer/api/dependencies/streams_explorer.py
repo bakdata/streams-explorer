97b3b7ed4e10649a39b5f3707608d252b509239f
Salomon Popp <salomon.popp@bakdata.com>
2022-07-14 17:52:50 +0200

    Improve type annotations for FastAPI request dependency

--- backend/streams_explorer/api/dependencies/streams_explorer.py
+++ backend/streams_explorer/api/dependencies/streams_explorer.py
@@ -9,16 +9,6 @@ from starlette.websockets import WebSocket
 from streams_explorer.streams_explorer import StreamsExplorer
 
 
-@overload
-def get_application(*, request: Request, websocket: WebSocket | None = None) -> FastAPI:
-    ...
-
-
-@overload
-def get_application(*, request: Request | None = None, websocket: WebSocket) -> FastAPI:
-    ...
-
-
 def get_application(
     *, request: Request | None = None, websocket: WebSocket | None = None
 ) -> FastAPI:
@@ -29,9 +19,21 @@ def get_application(
     raise ValueError("no request or websocket argument given")
 
 
-def get_streams_explorer(  # FastAPI complains with proper type annotations (Optional[Request])
-    *, request: Request = None, websocket: WebSocket = None  # type: ignore
+@overload
+def get_streams_explorer(
+    *, request: Request, websocket: None = None
 ) -> StreamsExplorer:
+    ...
+
+
+@overload
+def get_streams_explorer(
+    *, request: None = None, websocket: WebSocket
+) -> StreamsExplorer:
+    ...
+
+
+def get_streams_explorer(*, request=None, websocket=None) -> StreamsExplorer:
     app = get_application(request=request, websocket=websocket)
     return get_streams_explorer_from_state(app)
 
