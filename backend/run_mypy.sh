422edcf5763637e6208de3dfd80d005596be7bc7
Salomon Popp <salomon.popp@bakdata.com>
2022-07-14 17:51:45 +0200

    Refactor MyPy hook using custom script

---
+++ backend/run_mypy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# from https://jaredkhan.com/blog/mypy-pre-commit
+
+set -o errexit
+
+# change directory to the project root directory.
+cd "$(dirname "$0")"
+
+mypy --pretty --package streams_explorer --namespace-packages
