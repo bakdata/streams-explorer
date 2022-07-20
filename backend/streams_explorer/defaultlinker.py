7626f9aafb7847a63029f73e29e2588636584a56
Salomon Popp <salomon.popp@bakdata.com>
2022-07-20 14:27:20 +0200

    Refactor defaultlinker with pattern matching

--- backend/streams_explorer/defaultlinker.py
+++ backend/streams_explorer/defaultlinker.py
@@ -53,36 +53,41 @@ class DefaultLinker(LinkingService):
     def get_redirect_connector(self, config: dict, link_type: str | None) -> str | None:
         if connector_name := config.get("name"):
             consumer_group = f"connect-{connector_name}"
-            if link_type == "grafana":
-                return f"{settings.grafana.url}/d/{settings.grafana.dashboards.consumergroups}?var-consumergroups={consumer_group}"
-            elif link_type == "akhq":
-                return f"{settings.akhq.url}/ui/{settings.akhq.cluster}/group/{consumer_group}"
-            elif link_type == "akhq-connect":
-                return f"{settings.akhq.url}/ui/{settings.akhq.cluster}/connect/{settings.akhq.get('connect')}/definition/{connector_name}/tasks"
-            elif link_type == "kowl":
-                return f"{settings.kowl.url}/groups/{consumer_group}"
+            match link_type:
+                case "grafana":
+                    return f"{settings.grafana.url}/d/{settings.grafana.dashboards.consumergroups}?var-consumergroups={consumer_group}"
+                case "akhq":
+                    return f"{settings.akhq.url}/ui/{settings.akhq.cluster}/group/{consumer_group}"
+                case "akhq-connect":
+                    return f"{settings.akhq.url}/ui/{settings.akhq.cluster}/connect/{settings.akhq.get('connect')}/definition/{connector_name}/tasks"
+                case "kowl":
+                    return f"{settings.kowl.url}/groups/{consumer_group}"
 
     def get_redirect_topic(self, topic_name: str, link_type: str | None) -> str | None:
-        if link_type == "grafana":
-            return f"{settings.grafana.url}/d/{settings.grafana.dashboards.topics}?var-topics={topic_name}"
-        elif link_type == "akhq":
-            return f"{settings.akhq.url}/ui/{settings.akhq.cluster}/topic/{topic_name}"
-        elif link_type == "kowl":
-            return f"{settings.kowl.url}/topics/{topic_name}"
+        match link_type:
+            case "grafana":
+                return f"{settings.grafana.url}/d/{settings.grafana.dashboards.topics}?var-topics={topic_name}"
+            case "akhq":
+                return (
+                    f"{settings.akhq.url}/ui/{settings.akhq.cluster}/topic/{topic_name}"
+                )
+            case "kowl":
+                return f"{settings.kowl.url}/topics/{topic_name}"
 
     def get_redirect_streaming_app(
         self, k8s_app: K8sApp, link_type: str | None
     ) -> str | None:
-        if link_type == "kibanalogs":
-            return f"{settings.kibanalogs.url}/app/discover#/?_a=(columns:!(message),query:(language:lucene,query:'kubernetes.labels.app: \"{k8s_app.name}\"'))"
-        elif link_type == "loki":
-            return f'{settings.loki.url}/explore?orgId=1&left=["now-1h","now","loki",{{"expr":"{{app=\\"{k8s_app.name}\\"}}"}}]'
-        elif consumer_group := k8s_app.consumer_group:
-            if link_type == "grafana":
+        consumer_group = k8s_app.consumer_group
+        match link_type:
+            case "kibanalogs":
+                return f"{settings.kibanalogs.url}/app/discover#/?_a=(columns:!(message),query:(language:lucene,query:'kubernetes.labels.app: \"{k8s_app.name}\"'))"
+            case "loki":
+                return f'{settings.loki.url}/explore?orgId=1&left=["now-1h","now","loki",{{"expr":"{{app=\\"{k8s_app.name}\\"}}"}}]'
+            case "grafana" if consumer_group:
                 return f"{settings.grafana.url}/d/{settings.grafana.dashboards.consumergroups}?var-consumergroups={consumer_group}"
-            elif link_type == "akhq":
+            case "akhq" if consumer_group:
                 return f"{settings.akhq.url}/ui/{settings.akhq.cluster}/group/{consumer_group}"
-            elif link_type == "kowl":
+            case "kowl" if consumer_group:
                 return f"{settings.kowl.url}/groups/{consumer_group}"
 
     def get_sink_source_redirects(
