0652d3e6893fad0604763afbd9d7dd3c78413ae2
Salomon Popp <salomon.popp@bakdata.com>
2022-09-21 22:20:58 +0200

    Fix import

--- backend/streams_explorer/linker.py
+++ backend/streams_explorer/linker.py
@@ -2,7 +2,7 @@ from loguru import logger
 
 from streams_explorer.core.services.linking_services import LinkingService
 from streams_explorer.defaultlinker import DefaultLinker
-from streams_explorer.extractors import load_plugin
+from streams_explorer.plugins import load_plugin
 
 
 def load_linker() -> type[LinkingService]:
