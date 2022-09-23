ec5898023dd0cd55ec21a8d1e935e0380d09cdf0
Victor Kuenstler <victor.kuenstler@bakdata.com>
2022-09-23 09:14:30 +0200

    Use error-handling-avro

--- demo-atm-fraud/src/main/java/com/bakdata/kafka/TransactionJoiner.java
+++ demo-atm-fraud/src/main/java/com/bakdata/kafka/TransactionJoiner.java
@@ -11,37 +11,37 @@ import org.apache.kafka.streams.kstream.KStream;
 
 public class TransactionJoiner extends KafkaStreamsApplication {
 
-  public static void main(final String[] args) {
-    startApplication(new com.bakdata.kafka.TransactionJoiner(), args);
-  }
+    public static void main(final String[] args) {
+        startApplication(new com.bakdata.kafka.TransactionJoiner(), args);
+    }
 
-  @Override
-  public void buildTopology(final StreamsBuilder builder) {
-    final KStream<String, Transaction> input = builder.stream(this.getInputTopics());
-    final KStream<String, Transaction> mapped = input
-        .map((k, v) -> KeyValue.pair(v.getAccountId(), v));
+    @Override
+    public void buildTopology(final StreamsBuilder builder) {
+        final KStream<String, Transaction> input = builder.stream(this.getInputTopics());
+        final KStream<String, Transaction> mapped = input
+                .map((k, v) -> KeyValue.pair(v.getAccountId(), v));
 
-    final KStream<String, JoinedTransaction> joined = mapped
-        .join(mapped,
-            (t1, t2) -> JoinedTransaction
-                .newBuilder()
-                .setTransaction1(t1)
-                .setTransaction2(t2)
-                .build(),
-            JoinWindows.of(Duration.ofMinutes(10)).before(Duration.ZERO));
+        final KStream<String, JoinedTransaction> joined = mapped
+                .join(mapped,
+                        (t1, t2) -> JoinedTransaction
+                                .newBuilder()
+                                .setTransaction1(t1)
+                                .setTransaction2(t2)
+                                .build(),
+                        JoinWindows.ofTimeDifferenceWithNoGrace(Duration.ofMinutes(10)).before(Duration.ZERO));
 
-    joined.to(this.getOutputTopic());
-  }
+        joined.to(this.getOutputTopic());
+    }
 
-  @Override
-  public String getUniqueAppId() {
-    return "streams-explorer-transactionjoiner-" + this.getOutputTopic();
-  }
+    @Override
+    public String getUniqueAppId() {
+        return "streams-explorer-transactionjoiner-" + this.getOutputTopic();
+    }
 
-  @Override
-  protected Properties createKafkaProperties() {
-    final Properties kafkaProperties = super.createKafkaProperties();
-    kafkaProperties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.StringSerde.class);
-    return kafkaProperties;
-  }
+    @Override
+    protected Properties createKafkaProperties() {
+        final Properties kafkaProperties = super.createKafkaProperties();
+        kafkaProperties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.StringSerde.class);
+        return kafkaProperties;
+    }
 }
