047c58f19c712b0a38bb2ef5ab1c0f1f58fab6cd
Salomon Popp <salomon.popp@bakdata.com>
2022-12-05 17:57:57 +0100

    Supply QueryClient in test

--- frontend/__tests__/App.test.tsx
+++ frontend/__tests__/App.test.tsx
@@ -9,9 +9,18 @@ import mockRouter from "next-router-mock";
 import singletonRouter from "next/router";
 import nock from "nock";
 import React from "react";
-import App from "../components/App";
+import { QueryClient, QueryClientProvider } from "react-query";
+import AppComponent from "../components/App";
 
 jest.mock("next/router", () => require("next-router-mock"));
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <AppComponent />
+  </QueryClientProvider>
+);
 
 // -- Mock GraphVisualization
 jest.mock("../components/graph/Visualization", () => {
