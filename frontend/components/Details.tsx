b2d692edd4eb2f9fb2d68adadfe6c23e2edc1bbe
Salomon Popp <salomon.popp@bakdata.com>
2022-12-05 18:08:10 +0100

    Update Details

--- frontend/components/Details.tsx
+++ frontend/components/Details.tsx
@@ -16,17 +16,17 @@ interface DetailsProps {
 }
 
 const Details = ({ nodeId }: DetailsProps) => {
-  const { data, loading, error } = useGetNodeInfoApiNodeNodeIdGet({
-    node_id: nodeId,
+  const { data, isLoading, isError } = useGetNodeInfoApiNodeNodeIdGet({
+    pathParams: { nodeId: nodeId },
   });
-  if (loading) {
+  if (isLoading) {
     return (
       <div className={style.loadingSpinnerContainer}>
         <Spin tip="Loading..." />
       </div>
     );
   }
-  if (!data || error) {
+  if (!data || isError) {
     return (
       <Alert
         data-testid="no-node-info"
@@ -82,14 +82,15 @@ const NodeInfoDetail = ({ infoListItem, nodeId }: NodeInfoDetailProps) => {
 };
 
 const LinkInfo = ({ infoListItem, nodeId }: NodeInfoDetailProps) => {
-  const { data: linkToService, loading } = useGetLinkingApiNodeLinkingNodeIdGet(
-    {
-      node_id: nodeId,
-      queryParams: { link_type: infoListItem.value as string },
-    }
-  );
+  const { data: linkToService, isLoading } =
+    useGetLinkingApiNodeLinkingNodeIdGet(
+      {
+        pathParams: { nodeId: nodeId },
+        queryParams: { link_type: infoListItem.value as string },
+      }
+    );
 
-  if (loading) {
+  if (isLoading) {
     return <Spin tip="Loading link..." />;
   }
   if (linkToService) {
