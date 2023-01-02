2370753743a5e2be56a1283da909925ab80b1840
Salomon Popp <salomon.popp@bakdata.com>
2023-01-02 17:32:36 +0100

    Create extractor for streams-bootstrap producers

---
+++ backend/streams_explorer/core/extractor/default/streams_bootstrap_producer.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from kubernetes_asyncio.client import V1beta1CronJob, V1Job
+
+import streams_explorer.core.k8s_app as k8s
+from streams_explorer.core.extractor.extractor import ProducerAppExtractor
+
+if TYPE_CHECKING:
+    from streams_explorer.core.k8s_app import K8sAppJob
+
+
+class StreamsBootstrapProducer(ProducerAppExtractor):
+    def on_job_parsing(self, job: V1Job | V1beta1CronJob) -> K8sAppJob | None:
+        return k8s.K8sAppJob(job)
