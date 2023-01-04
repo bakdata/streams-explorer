753354a056f9b09cc711e28383fa59e6e2754733
Salomon Popp <salomon.popp@bakdata.com>
2023-01-04 17:03:28 +0100

    Expand test

--- backend/tests/test_kubernetes.py
+++ backend/tests/test_kubernetes.py
@@ -21,8 +21,16 @@ async def test_watch(kubernetes: Kubernetes, mocker: MockFixture):
     )
     mock_watch_namespace = mocker.spy(kubernetes, "_Kubernetes__watch_namespace")
 
-    mock_ctx = AsyncMock(side_effect=ApiException(status=410))
-    mock_kubernetes_asyncio_watch.return_value = mock_ctx
+    mock_Watch = AsyncMock(side_effect=ApiException(status=410, reason="Expired"))
+
+    mock_kubernetes_asyncio_watch.return_value = mock_Watch
+    # mock_ctx = AsyncMock(side_effect=ApiException(status=410, reason="Expired"))
+    # mock_Watch.__aenter__.return_value = mock_ctx
+    # mock_Watch.__aenter__.side_effect = ApiException(status=410, reason="Expired")
+    # mock_Watch.return_value.aenter.side_effect = ApiException(
+    #     status=410, reason="Expired"
+    # )
+    # mock_Watch.side_effect = ApiException(status=410, reason="Expired")
 
     await kubernetes.watch()
 
@@ -32,6 +40,17 @@ async def test_watch(kubernetes: Kubernetes, mocker: MockFixture):
         call("V1StatefulSet"),
         call("V1beta1CronJob"),
     ]
-    mock_ctx.__aenter__.assert_awaited()
-    assert mock_ctx.__aenter__.await_count == 3
+    mock_Watch.__aenter__.assert_awaited()
+    assert mock_Watch.__aenter__.await_count == 3
+
     assert mock_watch_namespace.call_count == 4
+    resources: list[str] = [
+        call.args[1].return_type.__name__
+        for call in mock_watch_namespace.call_args_list
+    ]
+    assert resources == [
+        "V1Deployment",
+        "V1StatefulSet",
+        "V1beta1CronJob",
+        "EventsV1Event",
+    ]
