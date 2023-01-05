fe7b607ff97bbc3b6990ce470d188117e6af18ef
Salomon Popp <salomon.popp@bakdata.com>
2023-01-05 12:19:16 +0100

    Expand test

--- backend/tests/test_kubernetes.py
+++ backend/tests/test_kubernetes.py
@@ -1,10 +1,10 @@
 from unittest.mock import AsyncMock, MagicMock, call
 
 import pytest
-from kubernetes_asyncio.client import ApiException
+from kubernetes_asyncio.client import ApiException, V1Deployment, V1DeploymentList
 from pytest_mock import MockFixture
 
-from streams_explorer.core.services.kubernetes import Kubernetes
+from streams_explorer.core.services.kubernetes import K8sResource, Kubernetes
 
 
 @pytest.fixture
@@ -24,13 +24,7 @@ async def test_watch(kubernetes: Kubernetes, mocker: MockFixture):
     mock_Watch = AsyncMock(side_effect=ApiException(status=410, reason="Expired"))
 
     mock_kubernetes_asyncio_watch.return_value = mock_Watch
-    # mock_ctx = AsyncMock(side_effect=ApiException(status=410, reason="Expired"))
-    # mock_Watch.__aenter__.return_value = mock_ctx
     # mock_Watch.__aenter__.side_effect = ApiException(status=410, reason="Expired")
-    # mock_Watch.return_value.aenter.side_effect = ApiException(
-    #     status=410, reason="Expired"
-    # )
-    # mock_Watch.side_effect = ApiException(status=410, reason="Expired")
 
     await kubernetes.watch()
 
@@ -54,3 +48,89 @@ async def test_watch(kubernetes: Kubernetes, mocker: MockFixture):
         "V1beta1CronJob",
         "EventsV1Event",
     ]
+
+
+@pytest.mark.asyncio
+async def test_watch_namespace(kubernetes: Kubernetes, mocker: MockFixture):
+    mock_kubernetes_asyncio_watch = mocker.patch(
+        "streams_explorer.core.services.kubernetes.kubernetes_asyncio.watch.Watch.stream"
+    )
+    mock_watch_namespace = mocker.spy(kubernetes, "_Kubernetes__watch_namespace")
+
+    mock_Watch = AsyncMock()
+    mock_kubernetes_asyncio_watch.return_value = mock_Watch
+    mock_Watch.__aenter__.return_value.stream.return_value.__aenter__.return_value = []
+
+    def mock_list_deployments() -> V1DeploymentList:
+        return V1DeploymentList()
+
+    async def mock_callback() -> None:
+        pass
+
+    await mock_watch_namespace(
+        "test-namespace",
+        K8sResource(
+            mock_list_deployments, return_type=V1Deployment, callback=mock_callback
+        ),
+    )
+
+    assert mock_kubernetes_asyncio_watch.call_count == 1
+
+
+@pytest.mark.asyncio
+async def test_watch_namespace_error(kubernetes: Kubernetes, mocker: MockFixture):
+    mock_kubernetes_asyncio_watch = mocker.patch(
+        "streams_explorer.core.services.kubernetes.kubernetes_asyncio.watch.Watch"
+    )
+    mock_watch_namespace = mocker.spy(kubernetes, "_Kubernetes__watch_namespace")
+
+    mock_Watch = AsyncMock()
+    mock_kubernetes_asyncio_watch.return_value = mock_Watch
+    mock_Watch.__aenter__.side_effect = ApiException(
+        status=500, reason="Internal Server Error"
+    )
+
+    def mock_list_deployments() -> V1DeploymentList:
+        return V1DeploymentList()
+
+    async def mock_callback() -> None:
+        pass
+
+    with pytest.raises(ApiException) as e:
+        await mock_watch_namespace(
+            "test-namespace",
+            K8sResource(
+                mock_list_deployments, return_type=None, callback=mock_callback
+            ),
+        )
+        assert e.value.status == 500
+        assert e.value.reason == "Internal Server Error"
+
+
+@pytest.mark.asyncio
+async def test_watch_namespace_restart(kubernetes: Kubernetes, mocker: MockFixture):
+    mock_kubernetes_asyncio_watch = mocker.patch(
+        "streams_explorer.core.services.kubernetes.kubernetes_asyncio.watch.Watch"
+    )
+    mock_watch_namespace = mocker.spy(kubernetes, "_Kubernetes__watch_namespace")
+
+    mock_Watch = AsyncMock()
+    mock_kubernetes_asyncio_watch.return_value = mock_Watch
+    mock_Watch.__aenter__.side_effect = ApiException(status=410, reason="Expired")
+
+    def mock_list_deployments() -> V1DeploymentList:
+        return V1DeploymentList()
+
+    async def mock_callback() -> None:
+        pass
+
+    with pytest.raises(RecursionError):
+        await mock_watch_namespace(
+            "test-namespace",
+            K8sResource(
+                mock_list_deployments, return_type=None, callback=mock_callback
+            ),
+        )
+
+    # assert mock_kubernetes_asyncio_watch.call_count == 1
+    # assert mock_kubernetes_asyncio_watch.call_args_list == [call("V1Deployment")]
