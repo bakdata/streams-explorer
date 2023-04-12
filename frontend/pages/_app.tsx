b6ca473dd2157790d578252ebf25e576ed2fab3e
Salomon Popp <salomon.popp@bakdata.com>
2023-04-12 13:02:37 +0200

    Fix import

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { AppProps } from "next/app";
 import { useState } from "react";
-import { ReactQueryDevtools } from "react-query/devtools";
 import "../styles/global.css";
 
 // This default export is required in a new `pages/_app.js` file.
