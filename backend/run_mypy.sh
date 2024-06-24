b1e407ee6c85a640426c8d713ad0f7ee05fe3b50
yordanovsstoyan <stoyan.yordanov@bakdata.com>
2024-06-24 11:32:06 +0300

    mypy

--- backend/run_mypy.sh
+++ backend/run_mypy.sh
@@ -6,4 +6,4 @@ set -o errexit
 # change directory to the project root directory.
 cd "$(dirname "$0")"
 
-mypy --pretty --package streams_explorer --package tests --namespace-packages --config-file mypy.ini
+mypy --pretty --package streams_explorer --package tests --namespace-packages
