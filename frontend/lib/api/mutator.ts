11e6039fa14d31b70e412e81c10c44883ce0ed14
Salomon Popp <salomon.popp@bakdata.com>
2026-07-08 10:55:32 +0200

    style: add comment to Axios mutator

--- frontend/lib/api/mutator.ts
+++ frontend/lib/api/mutator.ts
@@ -1,10 +1,11 @@
+// source https://orval.dev/docs/guides/custom-axios/#custom-instance-implementation
 import Axios, { AxiosError, AxiosRequestConfig } from "axios";
 
 export const AXIOS_INSTANCE = Axios.create();
 
 export const customInstance = <T>(
   config: AxiosRequestConfig,
-  options?: AxiosRequestConfig,
+  options?: AxiosRequestConfig
 ): Promise<T> => {
   return AXIOS_INSTANCE({ ...config, ...options }).then(({ data }) => data);
 };
